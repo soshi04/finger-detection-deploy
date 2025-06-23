84bb084f56ce5755587c985c2f12796d23a413a0
soshi04 <sirsohumjoshi@gmail.com>
2025-06-22 21:11:18 -0400

    fixed unused import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const App = () => {
   const videoRef = useRef<HTMLVideoElement>(null);
